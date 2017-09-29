c35715d0e9065280f5048ce356d32937ffa60361
antgerasim <antgerasim@gmail.com>
2017-09-29 17:01:12 +0300

    fixed errors after running nswag/refresh.bat

--- angular/src/app/users/create-user/create-user.component.ts
+++ angular/src/app/users/create-user/create-user.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, ViewChild, Injector, Output, EventEmitter, ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, Injector, Output, EventEmitter, ElementRef, OnInit } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap';
 import { UserServiceProxy, CreateUserDto, RoleDto } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/app-component-base';
@@ -38,7 +38,7 @@ export class CreateUserComponent extends AppComponentBase implements OnInit {
     show(): void {
         this.active = true;
         this.modal.show();
-        this.user = new CreateUserDto({ isActive: true });
+        this.user = new CreateUserDto(({ isActive: true }) as any);
     }
 
     onShown(): void {
